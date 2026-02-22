b29213fa09227940f31f2904b0cdc09eea4f8530
Ishmam90 <ishmamkabir90@email.com>
2026-02-22 16:47:55 +0600

    change

--- Lab 11/230041148_T01_L11_1B.cpp
+++ Lab 11/230041148_T01_L11_1B.cpp
@@ -1,47 +1,51 @@
-#include<iostream>
-#include<map>
-#include<list>
-#include<climits>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
 using namespace std;
 
-int main(){
+int main() {
     int a, b;
     map<int, list<int>> m;
 
-    cin>>a>>b;
+    cin >> a >> b;
 
-    for(int i=1; i<=a; i++) m[i].push_back(i);
+    for (int i = 1; i <= a; i++)
+        m[i].push_back(i);
 
-    int compMax=0, compMap=0;
+    int compMax = 0, compMap = 0;
 
-    while(b--){
-        int x, y, par1, par2, sizeMap, sizeMax=INT_MIN;
-        cin>>x>>y;
+    while (b--) {
+        int x, y, par1, par2, sizeMap, sizeMax = INT_MIN;
+        cin >> x >> y;
 
-        for(auto it:m)
-            for(auto it2:it.second){
-                if(it2==x)      par1=it.first;
-                else if(it2==y) par2=it.first;
+        for (auto it : m)
+            for (auto it2 : it.second) {
+                if (it2 == x)
+                    par1 = it.first;
+                else if (it2 == y)
+                    par2 = it.first;
             }
 
-        if(par1!=par2){
-            for(auto it:m[par2]){
+        if (par1 != par2) {
+            for (auto it : m[par2]) {
                 m[par1].push_back(par2);
             }
             m.erase(par2);
         }
 
-        sizeMap=m.size();
+        sizeMap = m.size();
 
-        for(auto it:m){
-            sizeMax=max(sizeMax, (int)it.second.size());
+        for (auto it : m) {
+            sizeMax = max(sizeMax, (int)it.second.size());
         }
 
-        if(sizeMap==compMap && sizeMax==compMax)    cout<<"No change"<<endl;
-        else{
-            cout<<sizeMap<<" "<<sizeMax<<endl;
-            compMap=sizeMap;
-            compMax=sizeMax;
+        if (sizeMap == compMap && sizeMax == compMax)
+            cout << "No change" << endl;
+        else {
+            cout << sizeMap << " " << sizeMax << endl;
+            compMap = sizeMap;
+            compMax = sizeMax;
         }
     }
 }
--- Lab 11/230041148_T02_L11_1B.cpp
+++ Lab 11/230041148_T02_L11_1B.cpp
@@ -1,33 +1,35 @@
-#include<iostream>
-#include<map>
-#include<list>
+#include <iostream>
+#include <list>
+#include <map>
 using namespace std;
 
-int main(){
+int main() {
     int a, b;
     map<int, list<int>> m;
 
-    cin>>a>>b;
+    cin >> a >> b;
 
-    for(int i=1; i<=a; i++) m[i].push_back(0);
+    for (int i = 1; i <= a; i++)
+        m[i].push_back(0);
 
-    while(b--){
+    while (b--) {
         int x, y;
-        cin>>x>>y;
+        cin >> x >> y;
 
         m[x].push_back(y);
         m[y].push_back(x);
     }
 
-    int edge=0;
-    
-    for(auto it:m){
-        if(edge==0) edge=it.second.size();
-        else if(edge!=it.second.size()){
-            cout<<"No"<<endl;
+    int edge = 0;
+
+    for (auto it : m) {
+        if (edge == 0)
+            edge = it.second.size();
+        else if (edge != it.second.size()) {
+            cout << "No" << endl;
             return 0;
         }
     }
 
-    cout<<"Yes"<<endl;
+    cout << "Yes" << endl;
 }
